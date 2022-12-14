810b84fdf97ee2721d5b711c5985ebad058e90bf
Joey Tsui <joeytsui131@gmail.com>
2022-12-14 16:18:08 -0500

    routes and controllers

--- route_and_controllers/app/controllers/users_controller.rb
+++ route_and_controllers/app/controllers/users_controller.rb
@@ -9,11 +9,36 @@ class UsersController < ApplicationController
         if user.save
         render json: user
         else
-            render json: user.errors.full_messages, status: :unprocessable_entity
+            render json: user.errors.full_messages, status: 422
         end
     end
 
     def show
-        render json: params
+        @user = User.find(params[:id])
+        render json: @user
+    end
+
+    def update
+        @user = User.find(params[:id])
+
+        if @user.update(user_params)
+            redirect_to user_url(@user)
+        else
+            render json: @user.errors.full_messages, status: 422
+        end
+    end
+
+    def destroy
+        @user = User.find(params[:id])
+
+        @user.destroy
+
+        redirect_to user_url
+    end
+
+    private
+    
+    def user_params
+        params.require(:user).permit(:name, :email)
     end
 end
